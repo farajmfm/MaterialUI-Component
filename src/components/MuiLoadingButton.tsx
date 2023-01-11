8fc4eb76456deb2a52d17bebaaf18c5a9b4f31b6
mohomed faraj <mfmfaraj@gmail.com>
2023-01-11 18:50:30 +0530

    third commit

--- src/components/MuiLoadingButton.tsx
+++ src/components/MuiLoadingButton.tsx
@@ -1,7 +1,16 @@
-
+import { Stack } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
+import SaveIcon from '@mui/icons-material/Save'
 
 export const MuiLoadingButton = () => {
     return (
-        <div>MUI Loading Button</div>
+        <Stack spacing={2} direction='row'>
+            <LoadingButton variant='outlined' >Submit</LoadingButton>
+            <LoadingButton loading variant='outlined' >Submit</LoadingButton>
+            <LoadingButton variant='outlined' loadingIndicator='Loading...'>Fetch Data</LoadingButton>
+            <LoadingButton loading variant='outlined' loadingIndicator='Loading...'>Fetch Data</LoadingButton>
+            <LoadingButton variant='outlined' loadingPosition='start' startIcon={< SaveIcon />}>Save</LoadingButton>
+            <LoadingButton loading variant='outlined' loadingPosition='start' startIcon={< SaveIcon />}>Save</LoadingButton>
+        </Stack>
     )
 }
